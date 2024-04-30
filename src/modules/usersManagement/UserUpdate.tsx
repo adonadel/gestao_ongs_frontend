d6c0b5702d03506322ee0d97c1ef22be7fc0e3c7
Antônio Zilli Zanolli <antoniozzanolli@gmail.com>
2024-04-29 21:06:33 -0300

    refactor: UserUpdate component to use dynamic mask based on input length

--- src/modules/usersManagement/UserUpdate.tsx
+++ src/modules/usersManagement/UserUpdate.tsx
@@ -30,16 +30,32 @@ interface CustomProps {
 const TextMaskCustom = React.forwardRef<HTMLInputElement, CustomProps>(
     function TextMaskCustom(props, ref) {
         const { onChange, ...other } = props;
+        const [mask, setMask] = useState('');
+
+        const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            const inputValue = event.target.value.replace(/[^\d]/g, '');
+            const inputLength = inputValue.length;
+
+            if (inputLength <= 11) {
+                setMask('000.000.000-000');
+            } else {
+                setMask('00.000.000/0000-00');
+            }
+
+            onChange({ target: { name: props.name, value: inputValue } });
+        }
+
         return (
             <IMaskInput
                 {...other}
-                mask="00.000.000/0000-00"
+                mask={mask}
                 definitions={{
                     '#': /[1-9]/,
                 }}
                 inputRef={ref}
                 onAccept={(value: string) => onChange({ target: { name: props.name, value } })}
                 overwrite
+                onChange={handleChange}
             />
         );
     },
