b9260fd7cf7bcbfd367a1a8ec700df700c83dee6
Éric <nascimento3ric@gmail.com>
2024-06-03 21:46:08 -0300

    Adiciona picker de imagem de animis, post bulk

--- src/modules/admin/animalsManagement/AnimalUpdate.tsx
+++ src/modules/admin/animalsManagement/AnimalUpdate.tsx
@@ -1,10 +1,12 @@
-import { Button, TextField, Grid } from '@mui/material';
+import { Button, TextField, Grid, Avatar, Box, IconButton } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getToken } from '../../../shared/utils/getToken';
 import { Animal } from './types';
+import { Loading } from '../../../shared/components/loading/Loading';
+import { Filter } from '@mui/icons-material';
 
 const AnimalUpdate: React.FC = () => {
     const navigate = useNavigate();
@@ -13,40 +15,41 @@ const AnimalUpdate: React.FC = () => {
     const [animal, setAnimal] = useState<Animal | null>(null);
     const { register, handleSubmit } = useForm<Animal>();
     const isEditMode = !!id;
+    const [isLoading, setIsLoading] = useState(false);
+    const [images, setImages] = useState<any>();
+    const imageUrl = import.meta.env.VITE_URL_IMAGE;
+
+    const openImagePicker = () => {
+        document.getElementById('imagePicker')?.click();
+    }
 
 
     const handleImageChange = async (e: any) => {
         const files: File[] = Array.from(e.target.files);
         const formData = new FormData();
-    
+
         files.forEach((file, index) => {
             formData.append(`medias[${index}][media]`, file);
-            formData.append(`medias[${index}][display_name]`, `Display Name ${index}`); // Substitua por valores reais
-            formData.append(`medias[${index}][description]`, `Description ${index}`); // Substitua por valores reais
-            formData.append(`medias[${index}][order]`, `${index}`); // Substitua por valores reais
         });
-    
+
         const token = getToken();
-    
-        console.log("Files and metadata being sent:", files); // Adicionando log
-    
+        setIsLoading(true);
         try {
             const response = await axios.post(`${apiUrl}/api/medias/bulk`, formData, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                     'Content-Type': 'multipart/form-data',
                 }
-            });
-    
-            console.log("Response from server:", response);
+            })
+
+            setImages(response.data);
+
         } catch (error) {
             console.log("Error:", error);
+        } finally {
+            setIsLoading(false);
         }
     };
-    
-
-
-
 
     useEffect(() => {
         if (isEditMode) {
@@ -99,14 +102,14 @@ const AnimalUpdate: React.FC = () => {
         <>
             <form onSubmit={handleSubmit(onSubmit)} noValidate>
                 <Grid container spacing={2} sx={{
-                        border: 'solid 0.5px',
-                        borderColor: 'primary.light',
-                        boxShadow: '0px 4px 4px rgba(55, 55, 55, 0.25)',
-                        padding: '20px',
-                        borderRadius: '10px'
-                    }}>
+                    border: 'solid 0.5px',
+                    borderColor: 'primary.light',
+                    boxShadow: '0px 4px 4px rgba(55, 55, 55, 0.25)',
+                    padding: '20px',
+                    borderRadius: '10px'
+                }}>
 
-                    <Grid item xs={6} spacing={2}>
+                    <Grid item xs={12}>
 
                         <TextField
                             type="hidden"
@@ -121,14 +124,14 @@ const AnimalUpdate: React.FC = () => {
                             defaultValue={isEditMode ? animal?.name : 'Alfredo'}
                         />
                     </Grid>
-                    <Grid item xs={6} spacing={0}>
+                    <Grid item xs={6}>
                         <TextField
                             label="Espécie"
                             type="text"
-                            defaultValue={'Cachorro'}
+                            defaultValue={'DOG'}
                         />
                     </Grid>
-                    <Grid item xs={4} spacing={0}>
+                    <Grid item xs={6}>
                         <TextField
                             label="Gênero"
                             type="text"
@@ -136,7 +139,7 @@ const AnimalUpdate: React.FC = () => {
                             defaultValue={isEditMode ? animal?.gender : 'MALE'}
                         />
                     </Grid>
-                    <Grid item xs={4} spacing={0}>
+                    <Grid item xs={6}>
                         <TextField
                             label="Tamanho"
                             type="text"
@@ -144,7 +147,7 @@ const AnimalUpdate: React.FC = () => {
                             defaultValue={isEditMode ? animal?.size : 'SMALL'}
                         />
                     </Grid>
-                    <Grid item xs={4} spacing={0}>
+                    <Grid item xs={6}>
                         <TextField
                             label="Idade"
                             type="text"
@@ -153,34 +156,16 @@ const AnimalUpdate: React.FC = () => {
                         />
 
                     </Grid>
-
-                    <Grid item xs={6} spacing={0}>
-                        <TextField
-                            label="Descrição"
-                            type="text"
-                            {...register('description')}
-                            defaultValue={isEditMode ? animal?.description : 'Lorem ipsum'}
-                        />
-                    </Grid>
-                    <Grid item xs={6} spacing={0}>
+                    <Grid item xs={6}>
                         <TextField
                             label="Castração"
                             type="text"
                             {...register('castrate_type')}
                             defaultValue={isEditMode ? animal?.castrate_type : 'CASTRATED'}
                         />
-
                     </Grid>
 
-                    <Grid item xs={6} spacing={0}>
-                        <TextField
-                            label="Localização do animal"
-                            type="text"
-                            {...register('location')}
-                            defaultValue={isEditMode ? animal?.location : 'centro, rua tal de tal'}
-                        />
-                    </Grid>
-                    <Grid item xs={6} spacing={0}>
+                    <Grid item xs={6}>
                         <TextField
                             label="Tags"
                             type="text"
@@ -189,14 +174,83 @@ const AnimalUpdate: React.FC = () => {
                         />
                     </Grid>
 
-                    <input type="file" accept='image/*' multiple onChange={handleImageChange} />
 
-                    <Button type='button' variant='contained' color="primary" onClick={() => navigate(-1)}>
-                        Voltar
-                    </Button>
-                    <Button type='submit' variant='contained' color="primary">
-                        {isEditMode ? 'Salvar' : 'Criar'}
-                    </Button>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Descrição"
+                            type="text"
+                            {...register('description')}
+                            defaultValue={isEditMode ? animal?.description : 'Lorem ipsum'}
+                        />
+                    </Grid>
+
+
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Localização do animal"
+                            type="text"
+                            {...register('location')}
+                            defaultValue={isEditMode ? animal?.location : 'centro, rua tal de tal'}
+                        />
+                    </Grid>
+
+                    <input id='imagePicker' type="file" accept='image/*' multiple onChange={handleImageChange} style={{ visibility: 'hidden', height: '0', width: '0' }} />
+
+
+
+                    <Grid item xs={12}>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+
+
+                            {Array.isArray(images) &&
+                                images.map((image: any, index: number) => (
+                                    <Avatar
+                                        key={index}
+                                        alt="Imagem"
+                                        src={imageUrl+image.filename_id}
+                                        variant='rounded'
+                                        sx={{ width: '6rem', height: '6rem' }}
+                                    />
+                                ))
+                            }
+
+                            <IconButton
+                                onClick={openImagePicker}
+                                color="secondary"
+                                sx={{
+                                    border: '1px solid',
+                                    borderColor: 'primary.dark',
+                                    borderRadius: '4px',
+                                    width: '6rem',
+                                    height: '6rem'
+                                }}>
+                                <Filter />
+                            </IconButton>
+                        </Box>
+                    </Grid>
+
+
+
+                    <Grid item xs={12} sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '0.5rem'
+                    }}>
+                        <Button type='button' size='large' variant='contained' color="primary" onClick={() => navigate(-1)} sx={{ width: '20%' }} disabled={isLoading}>
+                            Voltar
+                        </Button>
+                        <Button type='submit' size='large' variant='contained' color="success" sx={{ width: '80%' }} disabled={isLoading}>
+                            {isEditMode ? 'Salvar' : 'Criar'}
+                        </Button>
+                    </Grid>
+
+
+                    {
+                        isLoading && (
+                            <Loading />
+                        )
+                    }
+
                 </Grid>
             </form>
         </>
