da385c220086b2b96b041583bf85ab733debb34e
Éric <nascimento3ric@gmail.com>
2024-06-11 23:44:52 -0300

    Adiciona layout de banner para etapa de site da ONG

--- src/shared/components/header/DefaultHeader.tsx
+++ src/shared/components/header/DefaultHeader.tsx
@@ -1,6 +1,6 @@
 import { Person } from '@mui/icons-material';
 import LoginIcon from '@mui/icons-material/Login';
-import { Typography } from '@mui/material';
+import { Avatar, Box, Typography } from '@mui/material';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
@@ -55,25 +55,106 @@ const IsLogged = () => {
 const DefaultHeader = (props: IHeaderProps) => {
 
     return (
-        <AppBar position="sticky" open={props.open} sx={{ marginBottom: '32px' }} >
-            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                {!props.open && (
-                    <Typography
-                        component={Link}
-                        to='/'
-                        sx={{
-                            fontSize: '1.5625rem',
-                            fontWeight: 600,
-                            color: 'secondary.main',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        EBAA Patinhas
+        <>
+
+            <AppBar position="sticky" open={props.open} >
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    {!props.open && (
+                        <Typography
+                            component={Link}
+                            to='/'
+                            sx={{
+                                fontSize: '1.5625rem',
+                                fontWeight: 600,
+                                color: 'secondary.main',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            EBAA Patinhas
+                        </Typography>
+                    )}
+                    <IsLogged />
+                </Toolbar>
+            </AppBar>
+
+
+            <Box sx={{
+                position: 'relative',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'left',
+                justifyContent: 'center',
+                gap: '2rem',
+                height: '400px',
+                width: '100vw',
+                backgroundColor: 'background.default',
+                color: 'text.primary',                
+            }}>
+
+
+                <Typography variant="h5" component="h2" gutterBottom sx={{
+                    zIndex: 1,
+                    color: 'primary.main',
+                    fontWeight: 800,
+                    fontSize: '1.8rem',
+                    marginLeft: '4rem',
+                }}>
+                    "Amor pela vida, <br /> amor por amparar."
+                </Typography>
+
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '1rem',
+                    marginLeft: '4rem',
+                    zIndex: 1,
+                }}>
+                    <Typography variant="h1" component="h1" sx={{
+                        color: 'primary.main',
+                        display: 'flex',
+                        gap: '1rem',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+
+                        <Avatar src='public\logoPatinhas.svg' sx={{
+                            width: '4rem',
+                            height: '4rem',
+                        }} />
+
+                        <Typography variant="h2" component="h3">
+                            <Typography variant="h5" component="h3" sx={{
+                                fontWeight: 800,
+                                color: 'primary.main',
+                            }}>
+                                Patinhas Carentes
+                            </Typography>
+
+                            <Typography sx={{
+                                color: 'primary.main',
+                                fontWeight: 500,                                
+                            }}>
+                                Braço do Norte - SC
+                            </Typography>
+
+                        </Typography>
+
                     </Typography>
-                )}
-                <IsLogged />
-            </Toolbar>
-        </AppBar>
+
+                </Box>
+                <img src="public\background-header.jpg" alt="Banner" style={{
+                    position: 'absolute',
+                    marginBottom: '32px',
+                    objectFit: 'cover',
+                    width: '100%',
+                    height: '100%',
+                    maxWidth: '100vw',
+                    userSelect: 'none',
+                }} />
+
+            </Box>
+        </>
     );
 };
 
