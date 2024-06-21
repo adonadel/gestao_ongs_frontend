4f1800f93d5e79ed6be07210d6ea07b48a42dd30
Alexandre Donadel <xanded.fer2@gmail.com>
2024-06-20 23:37:30 -0300

    ebaa-124: continuando código da página de dashboard interna

--- src/modules/admin/dashboard/Dashboard.tsx
+++ src/modules/admin/dashboard/Dashboard.tsx
@@ -1,7 +1,6 @@
-import {Box, Container, Grid, Typography} from "@mui/material";
+import {Box, Container, FormControl, Grid, InputLabel, MenuItem, Select, Typography} from "@mui/material";
 import {useEffect, useState} from "react";
 import LineChart from "../../../shared/components/charts/LineChart.tsx";
-import {AxiosResponse} from "axios";
 import {baseApi} from "../../../lib/api.ts";
 
 
@@ -10,40 +9,19 @@ function Dashboard() {
   const [totalFinances, setTotalFinances] = useState(null);
   const [totalAnimals, setTotalAnimals] = useState(null);
   const [totalAnimalsCastration, setTotalAnimalsCastration] = useState(null);
+  const [dashboardType, setDashboardType] = useState('yearly');
   
-
-  const fetchTotalFinance = async () => {
+  const fetchDashboardData = async () => {
     setIsLoading(true);
     try {
-      const response: AxiosResponse = await baseApi.get(`/api/dashboards/finances`);
-      const totalFinancess = response.data;
-      setTotalFinances(totalFinancess);
+      const financeResponse = await baseApi.get(`/api/dashboards/finances?type=${dashboardType}`);
+      const animalsResponse = await baseApi.get(`/api/dashboards/animals?type=${dashboardType}`);
+      const castrationResponse = await baseApi.get(`/api/dashboards/animals/castration?type=${dashboardType}`);
+      setTotalFinances(financeResponse.data);
+      setTotalAnimals(animalsResponse.data);
+      setTotalAnimalsCastration(castrationResponse.data);
     } catch (error) {
-      console.error('Error fetching events:', error);
-    }
-    setIsLoading(false);
-  };
-  
-  const fetchTotalAnimals = async () => {
-    setIsLoading(true);
-    try {
-      const response: AxiosResponse = await baseApi.get(`/api/dashboards/animals`);
-      const totalAnimals = response.data;
-      setTotalAnimals(totalAnimals);
-    } catch (error) {
-      console.error('Error fetching events:', error);
-    }
-    setIsLoading(false);
-  };
-  
-  const fetchTotalAnimalsCastration = async () => {
-    setIsLoading(true);
-    try {
-      const response: AxiosResponse = await baseApi.get(`/api/dashboards/animals/castration`);
-      const totalAnimalsCastration = response.data;
-      setTotalAnimalsCastration(totalAnimalsCastration);
-    } catch (error) {
-      console.error('Error fetching events:', error);
+      console.error('Error fetching dashboard data:', error);
     }
     setIsLoading(false);
   };
@@ -55,12 +33,10 @@ function Dashboard() {
       currency: 'BRL'
     }).format(value);
   }
-
+  
   useEffect(() => {
-    fetchTotalFinance();
-    fetchTotalAnimals();
-    fetchTotalAnimalsCastration();
-  }, []);
+    fetchDashboardData();
+  }, [dashboardType]);
 
   return (
     <Container maxWidth="xl" style={{marginTop: '4rem'}}>
@@ -68,10 +44,33 @@ function Dashboard() {
         <p>Carregando...</p>
         :
           <Grid>
-            <Typography variant="h3" fontSize={'1rem'} fontWeight={'medium'} marginBottom={'1rem'}>Relação de entradas e saídas</Typography>
+            <Grid container justifyContent="space-between" alignItems="center" marginBottom={"1rem"}>
+              <Grid item> 
+                <Typography variant="h3" fontSize={'1rem'} fontWeight={'medium'} marginBottom={'1rem'}>Relação de entradas e saídas</Typography>
+              </Grid>
+              <Grid xs={2} item>
+                <FormControl fullWidth>
+                    <InputLabel id="especie-label">Período</InputLabel>
+                    <Select
+                        label="Período"
+                        labelId="type-label"
+                        id="type"
+                        value={dashboardType}
+                        onChange={(e) => { setDashboardType(e.target.value as string) }}
+                    >
+                        <MenuItem value="yearly" selected>Ano corrente</MenuItem>
+                        <MenuItem value="monthly">Mês corrente</MenuItem>
+                        <MenuItem value="weekly">Semana corrente</MenuItem>
+                        <MenuItem value="all">Período completo</MenuItem>
+                    </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={12} md={6} lg={6}>
-                <LineChart/>
+                <LineChart
+                  bottomType={dashboardType}
+                />
               </Grid>
               <Grid item xs={4} sm={4} md={2} lg={2}>
                 <Box sx={
